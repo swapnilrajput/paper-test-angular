dad29336b28bbdad687d66bcb55903874bc75f2b
bob <rajputswapnil91@gmail.com>
2020-12-29 03:34:50 +0530

    removed unused code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
-import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 const routes: Routes = [
   {
